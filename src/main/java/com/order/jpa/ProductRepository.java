71650d6dc17b0f4ebac01ba0e0fb97dca4bc3bb0
krishna91g <59868250+krishna91g@users.noreply.github.com>
2020-08-20 07:09:33 +0530

    Update ProductRepository.java

--- src/main/java/com/order/jpa/ProductRepository.java
+++ src/main/java/com/order/jpa/ProductRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.repository.CrudRepository;
 import com.order.entity.Products;
 
 public interface ProductRepository extends CrudRepository<Products, Long>{
-
+  public Products findByProductCode(String productCode);
 }
